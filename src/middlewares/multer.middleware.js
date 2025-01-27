afe129ff72c3cbced3a65282a0398722149a3dbd
suvamthakur <suvamthakur2005@gmail.com>
2025-01-28 01:11:00 +0530

    modify multer

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,8 +1,15 @@
+const fs = require("fs");
+const path = require("path");
 const multer = require("multer");
+
+const uploadDir = path.join(__dirname, "./public/temp");
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, { recursive: true });
+}
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "./public/temp");
+    cb(null, uploadDir);
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
